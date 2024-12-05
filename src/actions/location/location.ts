4edbe1a84b8242846cd7dcefbb08d630cfd575da
JEspinoza <soptek@gmail.com>
2024-12-04 16:40:27 -0800

    Modificaciones Funcionales Por el Momento
    Modificaciones Funcionales Por el Momento

--- src/actions/location/location.ts
+++ src/actions/location/location.ts
@@ -11,35 +11,47 @@ export const getCurrentLocation = async (): Promise<Location> => {
         });
       },
       error => {
-        console.log(`Can't get location`);
-        reject(error);
+        console.error('Error obteniendo la ubicación:', error); // Detalles del error
+        reject(error); // Pasar el error real
       },
       {
         enableHighAccuracy: true,
+        timeout: 20000, // Establecer un tiempo de espera para evitar que se quede colgado mucho tiempo
+        maximumAge: 1000, // Evitar obtener ubicaciones desactualizadas
       },
     );
   });
 };
 
+
 export const watchCurrentLocation = (
   locationCallback: (location: Location) => void,
 ): number => {
-  return Geolocation.watchPosition( info => (
-
-    locationCallback({
-      latitude: info.coords.latitude,
-      longitude: info.coords.longitude,
-    })
-
-
-  ), (error) => {
-    throw new Error(`Can't get watchPosition`);
-  }, {
-    enableHighAccuracy: true,
-  });
+  return Geolocation.watchPosition(
+    info => {
+      locationCallback({
+        latitude: info.coords.latitude,
+        longitude: info.coords.longitude,
+      });
+    },
+    error => {
+      console.error('Error observando la ubicación:', error); // Log del error
+      // Aquí podrías lanzar una alerta o manejar el error de otra manera, si lo deseas
+    },
+    {
+      enableHighAccuracy: true,
+      distanceFilter: 10, // Solo actualizar si el dispositivo se mueve una distancia mínima de 10 metros
+      interval: 15000, // Actualizar cada 15 segundos
+    },
+  );
 };
 
 
 export const clearWatchLocation = (watchId: number) => {
-  Geolocation.clearWatch(watchId);
-}
+  if (watchId !== null && watchId !== undefined) {
+    Geolocation.clearWatch(watchId);
+    console.log('Watch location cleared');
+  } else {
+    console.error('Invalid watchId');
+  }
+};
